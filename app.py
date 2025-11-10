3077a565608ee85115b4567dcc5ec411d5e5af4b
SamSpeedX <samochuu@gmail.com>
2025-11-10 15:03:23 +0300

    simple

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Render an HTML template
+    return render_template('index.html')
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form.get('name')
+    return render_template('result.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
